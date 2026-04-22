b8f61540faba26e71c54315addf40438f840a832
Jason Pearson <jason.d.pearson@gmail.com>
2026-04-22 09:01:27 -0500

    Migrate WrongConstant to AST arguments

--- tests/fixtures/negative/android-lint/WrongConstant.kt
+++ tests/fixtures/negative/android-lint/WrongConstant.kt
@@ -1,7 +1,31 @@
 package test
 
-import android.view.View
+annotation class IntDef(vararg val value: Int, val flag: Boolean = false)
 
-fun hideView(view: View) {
+@IntDef(View.VISIBLE, View.INVISIBLE, View.GONE)
+annotation class Visibility
+
+open class View {
+    companion object {
+        const val VISIBLE = 0
+        const val INVISIBLE = 4
+        const val GONE = 8
+    }
+
+    fun setVisibility(@Visibility value: Int) {}
+}
+
+class Fake {
+    fun setVisibility(value: Int) {}
+}
+
+const val LOCAL_VISIBLE = 0
+
+fun configure(view: View, fake: Fake) {
     view.setVisibility(View.GONE)
+    view.setVisibility(LOCAL_VISIBLE)
+    fake.setVisibility(0)
+    unresolved.setVisibility(0)
+    val text = "view.setVisibility(0)"
+    // view.setVisibility(0)
 }
--- tests/fixtures/positive/android-lint/WrongConstant.kt
+++ tests/fixtures/positive/android-lint/WrongConstant.kt
@@ -1,7 +1,64 @@
 package test
 
-import android.view.View
+annotation class IntDef(vararg val value: Int, val flag: Boolean = false)
 
-fun hideView(view: View) {
-    view.setVisibility(1)
+@IntDef(View.VISIBLE, View.INVISIBLE, View.GONE)
+annotation class Visibility
+
+@IntDef(View.LAYOUT_DIRECTION_LTR, View.LAYOUT_DIRECTION_RTL)
+annotation class LayoutDirection
+
+@IntDef(View.IMPORTANT_FOR_ACCESSIBILITY_AUTO, View.IMPORTANT_FOR_ACCESSIBILITY_YES, View.IMPORTANT_FOR_ACCESSIBILITY_NO)
+annotation class ImportantForAccessibility
+
+@IntDef(Gravity.LEFT, Gravity.RIGHT, Gravity.CENTER, flag = true)
+annotation class GravityInt
+
+@IntDef(LinearLayout.HORIZONTAL, LinearLayout.VERTICAL)
+annotation class Orientation
+
+open class View {
+    companion object {
+        const val VISIBLE = 0
+        const val INVISIBLE = 4
+        const val GONE = 8
+        const val LAYOUT_DIRECTION_LTR = 0
+        const val LAYOUT_DIRECTION_RTL = 1
+        const val IMPORTANT_FOR_ACCESSIBILITY_AUTO = 0
+        const val IMPORTANT_FOR_ACCESSIBILITY_YES = 1
+        const val IMPORTANT_FOR_ACCESSIBILITY_NO = 2
+    }
+
+    fun setVisibility(@Visibility value: Int) {}
+    fun setLayoutDirection(@LayoutDirection value: Int) {}
+    fun setImportantForAccessibility(@ImportantForAccessibility value: Int) {}
+}
+
+object Gravity {
+    const val LEFT = 3
+    const val RIGHT = 5
+    const val CENTER = 17
+}
+
+class TextView : View() {
+    fun setGravity(@GravityInt value: Int) {}
+}
+
+class LinearLayout : View() {
+    companion object {
+        const val HORIZONTAL = 0
+        const val VERTICAL = 1
+    }
+
+    fun setOrientation(@Orientation value: Int) {}
+}
+
+fun configure(view: View, textView: TextView, layout: LinearLayout) {
+    view.setVisibility(0)
+    view.setLayoutDirection(
+        1
+    )
+    view.setImportantForAccessibility(2)
+    textView.setGravity(17)
+    layout.setOrientation(1)
 }

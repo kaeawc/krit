a4bbf2541ab764cf29e3d17dca4d0642b0ce06d0
Jason Pearson <1414364+kaeawc@users.noreply.github.com>
2026-04-29 17:00:43 -0500

    Fix UseRequireNotNull complex condition matching (#658)

--- tests/fixtures/negative/style/UseRequireNotNull.kt
+++ tests/fixtures/negative/style/UseRequireNotNull.kt
@@ -17,3 +17,9 @@ fun validateCondition(x: Int) {
 fun validateEquality(x: Any?, y: Any?) {
     require(x != y)
 }
+
+fun validateComplexCondition(x: String?, y: String?) {
+    require(x != null && x.isNotBlank())
+    require(x != null || y != null) { "one value is required" }
+    require(null != x && y != null)
+}
